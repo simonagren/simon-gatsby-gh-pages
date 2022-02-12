290396abf768247c0289c34d6f2b0c4fb953ad61
Simon Ågren <simonagren@outlook.com>
2022-02-12 15:06:55 +0100

    commit message

---
+++ README.md
@@ -0,0 +1,74 @@
+# Simon Gatsby
+
+GitHub Action to build and deploy your Gatsby site to GitHub Pages
+
+## Usage
+
+This GitHub Action will run `gatsby build` at the root of your repository and
+deploy it to GitHub Pages for you! Here's a basic workflow example:
+
+```yml
+name: Gatsby Publish
+
+on:
+  push:
+    branches:
+      - dev
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v1
+      - uses: enriikke/gatsby-gh-pages-action@v2
+        with:
+          access-token: ${{ secrets.ACCESS_TOKEN }}
+```
+
+### Settings
+
+- **access-token**: A [GitHub Personal Access Token][github-access-token] with
+  the `repo` scope. This is **required** to push the site to your repo after
+  Gatsby finish building it. You should store this as a [secret][github-repo-secret]
+  in your repository. Provided as an [input][github-action-input].
+
+- **deploy-branch**: The branch expected by GitHub to have the static files
+  needed for your site. For org and user pages it should always be `master`.
+  This is where the output of `gatsby build` will be pushed to. Provided as an
+  [input][github-action-input].
+  Defaults to `master`.
+
+- **deploy-repo**: The repository expected by GitHub to have the static files
+  needed for your site.
+  Provided as an [input][github-action-input].
+  Defaults to the same repository that runs this action.
+
+- **gatsby-args**: Additional arguments that get passed to `gatsby build`. See the
+  [Gatsby documentation][gatsby-build-docs] for a list of allowed options.
+  Provided as an [input][github-action-input].
+  Defaults to nothing.
+
+- **skip-publish**: Builds your Gatsby site but skips publishing by setting it to `true`,
+  effectively performing a test of the build process using the live configuration.
+  Provided as an [input][github-action-input].
+  Defaults to **false**
+
+- **working-dir**: The directory where your Gatsby source files are at. `gatsby build`
+  will run from this directory.
+  Provided as an [input][github-action-input].
+  Defaults to the project's root.
+
+- **public-dir**: The public directory where the files are publishes. 
+  Provided as an [input]  [github-action-input].
+  Defaults to the Public folder in the project's root.
+  
+- **git-config-name**: Provide a custom name that is used to author the git commit, which
+  is pushed to the deploy branch.
+  Provided as an [input][github-action-input].
+  Defaults to the GitHub username of the action actor.
+
+- **git-config-email**: Provide a custom email that is used to author the git commit, which
+  is pushed to the deploy branch.
+  Provided as an [input][github-action-input].
+  Defaults to `{actor}@users.noreply.github.com`, where `{actor}` is the GitHub username 
+  of the action actor.
---
+++ index.ts
@@ -0,0 +1,102 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as github from '@actions/github'
+import * as io from '@actions/io'
+import * as ioUtil from '@actions/io/lib/io-util'
+
+const DEFAULT_DEPLOY_BRANCH = 'master'
+
+async function run(): Promise<void> {
+  try {
+    const accessToken = core.getInput('access-token')
+    if (!accessToken) {
+      core.setFailed(
+        'No personal access token found. Please provide one by setting the `access-token` input for this action.',
+      )
+      return
+    }
+
+    let deployBranch = core.getInput('deploy-branch')
+    if (!deployBranch) deployBranch = DEFAULT_DEPLOY_BRANCH
+
+    const deployRepo = core.getInput('deploy-repo')
+    const isSameRepo = !deployRepo || deployRepo === github.context.repo.repo
+
+    if (isSameRepo && github.context.ref === `refs/heads/${deployBranch}`) {
+      console.log(`Triggered by branch used to deploy: ${github.context.ref}.`)
+      console.log('Nothing to deploy.')
+      return
+    }
+
+    const workingDir = core.getInput('working-dir') || '.'
+    const publicDir = core.getInput('public-dir') || `${workingDir}/public`
+    const pkgManager = (await ioUtil.exists(`${workingDir}/yarn.lock`)) ? 'yarn' : 'npm'
+    console.log(`Installing your site's dependencies using ${pkgManager}.`)
+    await exec.exec(`${pkgManager} install`, [], { cwd: workingDir })
+    console.log('Finished installing dependencies.')
+
+    let gatsbyArgs = core.getInput('gatsby-args').split(/\s+/).filter(Boolean)
+    if (gatsbyArgs.length > 0) {
+      gatsbyArgs = ['--', ...gatsbyArgs]
+    }
+
+    console.log('Ready to build your Gatsby site!')
+    console.log(`Building with: ${pkgManager} run build ${gatsbyArgs.join(' ')}`)
+    await exec.exec(`${pkgManager} run build`, gatsbyArgs, { cwd: workingDir })
+    console.log('Finished building your site.')
+
+    const cnameExists = await ioUtil.exists(`${workingDir}/CNAME`)
+    if (cnameExists) {
+      console.log('Copying CNAME over.')
+      await io.cp(`${workingDir}/CNAME`, `${publicDir}/CNAME`, { force: true })
+      console.log('Finished copying CNAME.')
+    }
+
+    const skipPublish = (core.getInput('skip-publish') || 'false').toUpperCase()
+    if (skipPublish === 'TRUE') {
+      console.log('Building completed successfully - skipping publish')
+      return
+    }
+
+    const repo = `${github.context.repo.owner}/${deployRepo || github.context.repo.repo}`
+    const repoURL = `https://${accessToken}@github.com/${repo}.git`
+    console.log('Ready to deploy your new shiny site!')
+    console.log(`Deploying to repo: ${repo} and branch: ${deployBranch}`)
+    console.log('You can configure the deploy branch by setting the `deploy-branch` input for this action.')
+
+    await exec.exec(`git init`, [], { cwd: publicDir })
+
+    const gitUserName = core.getInput('git-config-name') || github.context.actor
+    const gitEmail = core.getInput('git-config-email') || `${github.context.actor}@users.noreply.github.com`
+
+    await exec.exec(`git config user.name`, [gitUserName], {
+      cwd: publicDir,
+    })
+    await exec.exec(`git config user.email`, [gitEmail], {
+      cwd: publicDir,
+    })
+
+    await exec.exec(`git add`, ['.'], { cwd: publicDir })
+
+    const commitMessageInput =
+      core.getInput('commit-message') || `deployed via Simon Gatsby Action ${github.context.sha}`
+    await exec.exec(`git commit`, ['-m', commitMessageInput], {
+      cwd: publicDir,
+    })
+
+    await exec.exec(`git push`, ['-f', repoURL, `master:${deployBranch}`], {
+      cwd: publicDir,
+    })
+    console.log('Finished deploying your site.')
+
+  } catch (err: any) {
+    core.setFailed(err.message)
+  }
+}
+
+// Don't auto-execute in the test environment
+if (process.env['NODE_ENV'] !== 'test') {
+  run()
+}
+
+export default run
